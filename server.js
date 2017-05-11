2744a7530782bc6d9e7952eb8988c792cd06c761
root <root@ip-172-31-26-167.us-west-2.compute.internal>
2017-05-11 02:51:44 +0000

    Remove c

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ var options = {
   // Certificate and key generating with commands
   // openssl req -newkey rsa:2048 -new -nodes -keyout key.pem -out csr.pem
   // openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out server.crt
-  key: fs.readFileSync('./key.pem', 'utf8'),
-  cert: fs.readFileSync('./server.crt', 'utf8')
+  key: fs.readFileSync('./certificate/key.pem', 'utf8'),
+  cert: fs.readFileSync('./certificate/server.crt', 'utf8')
 };
 
 // start the server
