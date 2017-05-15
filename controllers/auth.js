93400eb76c50c2e16470694e6e14bdc4e6d4fc8a
Raymond Perminov <raymondproduction@ukr.net>
2017-05-15 14:00:03 +0300

    Function for save of response and body

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ exports.forAccessToken = function(ctx, next) {
   // загружаем client_id, client_secret из файла config.json
   const client = require('./load_config');
   console.log(client.client_id);
-  console.log('code ',ctx.query.code);
+  console.log('code ', ctx.query.code);
   var request = require('request');
   request.post({
       url: 'https://github.com/login/oauth/access_token',
@@ -15,32 +15,32 @@ exports.forAccessToken = function(ctx, next) {
       }
     },
     function(error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-
-      console.log('access_token', response.access_token);
-      console.log('refresh_token', response.refresh_token);
-      console.log('token_type', response.token_type);
-      console.log('expires', response.exports);
-
-      console.log('access_token', body.access_token);
-      console.log('refresh_token', body.refresh_token);
-      console.log('token_type', body.token_type);
-      console.log('expires', body.exports);
+      console.log('response', response);
+      console.log('body', body);
       //  access_token, refresh_token, token_type, expires
+
+      fs.writeFile('response.txt', response, (err) => {
+        if (err) throw err;
+        console.log('The file respnse.txt has been saved!');
+      });
+
+      fs.writeFile('body.txt', response, (err) => {
+        if (err) throw err;
+        console.log('The file body.txt has been saved!');
+      });
+
     });
 
 
   //ctx.redirect('https://google.com.ua');
 }
 
-exports.mainPage = function(ctx){
+exports.mainPage = function(ctx) {
   var fs = require('fs');
   ctx.type = 'html'
   ctx.body = fs.createReadStream('views/oauth.html');
 }
 
-exports.accessToken = function(ctx, next){
-  console.log('request!!!! =>',ctx.request.body);
+exports.accessToken = function(ctx, next) {
+  console.log('request!!!! =>', ctx.request.body);
 }
